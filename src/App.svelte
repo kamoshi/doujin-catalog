bd69059c321b20f518bf6652c0518aab0ef825f8
Maciej Jur <mj@kamoshi.org>
2022-04-14 22:45:01 +0200

    Convert to typescript

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
-<script>
-	export let name;
+<script lang="ts">
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello !</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
